1af95d75b1584d84c5ed7420222d9ebebd10a29d
Mindgamesnl <matsmoolhuizen@gmail.com>
2020-06-16 23:16:30 +0200

    base setup

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
-package piper
+package main
+
+import (
+	"github.com/Mindgamesnl/piper/client"
+	"github.com/Mindgamesnl/piper/server"
+	"github.com/sirupsen/logrus"
+	"os"
+)
 
 func main() {
-	
+	args := os.Args[1:]
+
+	if len(args) == 0 {
+		logrus.Error("invalid arguments! use piper <client/server> <arguments>")
+		os.Exit(1)
+	}
+
+	if args[0] == "client" {
+		client.StartClient()
+		return
+	}
+
+	if args[0] == "server" {
+		server.StartServer()
+		return
+	}
+
+	logrus.Error("invalid arguments! use piper <client/server> <arguments>")
+	os.Exit(1)
 }
---
+++ scripts/build.sh
@@ -0,0 +1,4 @@
+export PATH=$PATH:$HOME/go/bin
+export GOPATH=$HOME/go;
+export PATH=$PATH:$GOPATH/bin;
+go install github.com/Mindgamesnl/piper
